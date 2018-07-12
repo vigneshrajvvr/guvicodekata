6abc424793644630b45b259c7e1009845e54e203
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-12 14:27:25 +0530

    Create set_4.py

---
+++ set_4.py
@@ -0,0 +1,6 @@
+ch = input()
+characters = ['a','e','i','o','u','A','E','I','O','U','b','c','d','f','g','h','p','j','k','l','m','n','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
+if(ch in characters):
+	print("Alphabet")
+else:
+    print("No")
