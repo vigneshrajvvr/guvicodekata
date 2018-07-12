8868c03cecf8d90a766e53cb524181f4222b3560
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-12 14:22:45 +0530

    Update set1-3.py

--- set1-3.py
+++ set1-3.py
@@ -1,6 +1,9 @@
 ch = input()
 vow = ['a','e','i','o','u','A','E','I','O','U']
+cons = ['b','c','d','f','g','h','p','j','k','l','m','n','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
 if(ch in vow):
 	print("Vowel")
+elif(ch in cons):
+	print("Consonant")
 else:
-	print("Consonant") 
+    print("Invalid")
