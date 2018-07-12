dde83a4a7d50f4a9cb701efcdde172e75278a43f
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-12 14:34:57 +0530

    Update set1_5.py

--- set1_5.py
+++ set1_5.py
@@ -1,10 +1,9 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if a or b or c <0:
-    if(a>b and a>c):
-        print(a)
-    elif(b>c):
-        print(b)
-    else:
-        print(c)
+if(a>b and a>c):
+    print(a)
+elif(b>c):
+    print(b)
+else:
+    print(c)
