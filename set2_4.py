f5d8bbe341022a7e9495bbd19d2c80277db77f2a
Vignesh Raj V <38003146+vigneshrajvvr@users.noreply.github.com>
2018-07-18 18:31:47 +0530

    Update set2_4.py

--- set2_4.py
+++ set2_4.py
@@ -2,7 +2,9 @@ N = int(input())
 Q = int(input())
 if N < Q:
     for i in range(N + 1, Q):
-        if i % 2 !=0:
+        if i % 2 ==0:
+            pass
+        else:
             print(i)
 else:
     print("Invalid Input") 
